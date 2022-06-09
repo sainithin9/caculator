3c8e6a42f2697b755757e3f3d28af5afdf651b4e
sainithin9 <sainithinpavurayala09042003@gmail.com>
2022-06-09 13:19:22 +0530

    d

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function rectangle(length,width){
 function circle(radius){
     return (Math.PI*radius*radius);
 }
+function trig(){
+    const v1=document.getElementById("nrm")
+    const v2=document.getElementById("tr")
+    const v3=document.getElementById("ars")
+    v2.style.display="block"
+    v3.style.display="none"
+    v1.style.display="none";
+}
+function areea(){
+    const v1=document.getElementById("nrm")
+    const v2=document.getElementById("tr")
+    const v3=document.getElementById("ars")
+    v2.style.display="none"
+    v3.style.display="block"
+    v1.style.display="none";
+}
