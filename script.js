e45ac19e05f08b36211b0db80af917023b28e7f9
sainithin9 <sainithinpavurayala09042003@gmail.com>
2022-06-09 10:43:35 +0530

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+function square(side){
+    return (side*side);
+}
+function triangle(height,base){
+    return (0.5*height*base);
+}
+function rectangle(length,width){
+    return (length*width);
+}
+function circle(radius){
+    return (Math.PI*radius*radius);
+}
